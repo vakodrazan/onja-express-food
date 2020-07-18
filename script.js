b62b8af7f283eada3823bb20c1fcf7fc7b4f91f2
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-07-18 09:58:55 +0300

    Create a form when click the add button

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
-console.log('good luck!');
+const addListBtn = document.querySelector('.add-order');
+const innerModal = document.querySelector('.inner-modal');
+const outerModal = document.querySelector('.outer-modal');
+
+
+const handleAddListBtn = (e) => {
+    const myFormHtml = `
+        <form>
+            <p>Your name :</p>
+            <input
+                class="input-form"
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Enter your name here"
+                required
+            />
+            <p>Please select your dish :</p>
+            <select name="dish" class="select-form" required>
+                <option value="romazava">Romazava</option>
+                <option value="koba">Koba</option>
+                <option value="ravitoto">Ravitoto</option>
+                <option value="mokary">Mokary</option>
+                <option value="achard">Achard</option>
+                <option value="masikita">Masikita</option>
+                <option value="sambos">Sambos</option>
+                <option value="mofo-baolina">Mofo baolina</option>
+                <option value="ranonapango">Ranonapango</option>
+            </select>
+            <p>Please select the size of your plate :</p>
+            <input type="radio" id="small" name="size" value="small" required />
+            <label for="small">Small</label><br />
+            <input type="radio" id="medium" name="size" value="medium" />
+            <label for="medium">Medium</label><br />
+            <input type="radio" id="large" name="size" value="large" />
+            <label for="large">Large</label><br />
+            <p>How many pieces do you want to order?</p>
+            <input
+                class="input-form"
+                type="number"
+                id="quantity"
+                name="amount"
+                placeholder="Enter a number here"
+                required
+            />
+            <button class="submitOrder" type="submit">Add this order</button>
+        </form>
+    `;
+
+    innerModal.innerHTML = myFormHtml;
+    outerModal.classList.add('open')
+}
+
+outerModal.addEventListener('click', event => {
+    const isOutside = !event.target.closest('.inner-modal')
+    if (isOutside) {
+        outerModal.classList.remove('open');
+    }
+});
+
+window.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        outerModal.classList.remove('open');
+    }
+});
+
+addListBtn.addEventListener('click', handleAddListBtn);
